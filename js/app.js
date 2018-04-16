5aa1a0c522cb02f8d23c60476d659525fcef1248
Mike Hodgson <mhodgson@gmail.com>
2018-04-16 09:41:22 -0400

    Initial code commit.

---
+++ js/app.js
@@ -0,0 +1,79 @@
+var vm = '';
+
+window.onload = function() {
+    Vue.component('v-select', VueSelect.VueSelect);
+    vm = new Vue({
+        el: '#app',
+        data: {
+            missions: _DEFAULT_MISSION_DATA,
+            activeMissions: [],
+            selectedMission: null
+        },
+        mounted : function() {
+            this.activeMissions = this.getActiveMissions();
+        },
+        updated : function() {
+            this.saveActiveMissions();
+        },
+        methods: {
+            handleOK (evt) {
+                if (this.selectedMission != null) {
+                    if (!this.hasActiveMission(this.activeMissions, this.selectedMission)) {
+                        this.activeMissions.push(this.selectedMission);
+                    }
+                    this.selectedMission = null;
+                }
+            },
+            getActiveMissions: function() {
+                var data = localStorage.getItem('activeMissions');
+                if (data != null) {
+                    return JSON.parse(data);
+                } else {
+                    return [];
+                }
+            },
+            saveActiveMissions: function(data) {
+                localStorage.setItem('activeMissions', JSON.stringify(this.activeMissions));
+            },
+            hasActiveMission(arr, obj) {
+                if (arr.filter(function(e) { return e.value === obj.value; }).length > 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            requirementIncrease: function(requirement) {
+                if (requirement.current < requirement.goal) 
+                    requirement.current += 1;
+            },
+            isRequirementComplete: function(requirement) {
+                if (requirement.current >= requirement.goal) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            areAllRequirementsComplete: function(mission) {
+                var complete = true;
+                var requirements = mission.requirements;
+                for (let i=0; i < requirements.length; i++) {
+                    if (requirements[i].goal > requirements[i].current) {
+                        complete = false;
+                    }
+                }
+                return complete;
+            },
+            removeActiveMission: function(arr, obj) {
+                for (let i=0; i < arr.length; i++) {
+                    if (arr[i].value = obj.value) {
+                        arr.splice(i, 1);
+                        break;
+                    }
+                }
+            },
+            clearActiveMissions: function() {
+                this.activeMissions = [];
+            }
+        }
+    });
+}
---
+++ js/data/missions.js
@@ -0,0 +1,204 @@
+var _DEFAULT_MISSION_DATA = [
+    {
+        value: 1,
+        label: "George Brett",
+        requirements: [
+            {
+                name: "H",
+                goal: 2,
+                current: 0
+            },
+            {
+                name: "2B",
+                goal: 1,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 2,
+        label: "J.D. Martinez",
+        requirements: [
+            {
+                name:"H",
+                goal: 2,
+                current: 0
+            },
+            {
+                name: "RBI",
+                goal: 2,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 3,
+        label: "Wade Davis",
+        requirements: [
+            {
+                name: "IP",
+                goal: 2,
+                current: 0
+            },
+            {
+                name: "K",
+                goal: 3,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 4,
+        label: "Shawn Green",
+        requirements: [
+            {
+                name: "H",
+                goal: 2,
+                current: 0
+            },
+            {
+                name: "SB",
+                goal: 1,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 5,
+        label: "Harold Baines",
+        requirements: [
+            {
+                name: "H",
+                goal: 1,
+                current: 0
+            },
+            {
+                name: "PO",
+                goal: 2,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 6,
+        label: "Brad Ziegler",
+        requirements: [
+            {
+                name: "HO",
+                goal: 1,
+                current: 0
+            },
+            {
+                name: "K",
+                goal: 2,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 7,
+        label: "Dale Murphy",
+        requirements: [
+            {
+                name: "H",
+                goal: 1,
+                current: 0
+            },
+            {
+                name: "R",
+                goal: 1,
+                current: 0
+            },
+            {
+                name: "SB",
+                goal: 1,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 8,
+        label: "Henderson Alvarez",
+        requirements: [
+            {
+                name: "W",
+                goal: 1,
+                current: 0
+            },
+            {
+                name: "IP",
+                goal: 3,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 9,
+        label: "Eric Karros",
+        requirements: [
+            {
+                name: "H",
+                goal: 2,
+                current: 0
+            },
+            {
+                name: "R",
+                goal: 1,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 10,
+        label: "Bill Mazeroski",
+        requirements: [
+            {
+                name: "H",
+                goal: 1,
+                current: 0
+            },
+            {
+                name: "PO",
+                goal: 1,
+                current: 0
+            },
+            {
+                name: "A",
+                goal: 1,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 11,
+        label: "Matt Kemp",
+        requirements: [
+            {
+                name: "H",
+                goal: 2,
+                current: 0
+            },
+            {
+                name: "SB",
+                goal: 1,
+                current: 0
+            }
+        ]
+    },
+    {
+        value: 12,
+        label: "Brandon Crawford",
+        requirements: [
+            {
+                name: "H",
+                goal: 1,
+                current: 0
+            },
+            {
+                name: "A",
+                goal: 2,
+                current: 0
+            }
+        ]
+    }
+];

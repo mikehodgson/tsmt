d2807ad02d72c6d0cbced687da4f83c694c7f8a9
Mike Hodgson <mhodgson@gmail.com>
2018-04-22 09:03:37 -0400

    Converted existing data to new format.

--- js/model/Mission.js
+++ js/model/Mission.js
@@ -1,24 +1,23 @@
 function Requirement(requirementData) {
-    this.requirement_type = requirementData.requirement_type;
-    this.goal = requirementData.goal;
-    this.current = requirementData.current || 0;
+    var _this = this;
+    _this.requirement_type = requirementData.requirement_type || requirementData.name || "";
+    _this.goal = requirementData.goal || 0;
+    _this.current = requirementData.current || 0;
 }
 
 function Mission(missionData) {
-    this.id = missionData.id;
-    this.player = missionData.player;
-    this.rating = missionData.rating;
-    this.series = missionData.series;
-    this.duration = missionData.duration;
-    this.team = missionData.team;
-    this.position = missionData.position;
-    this.program_type = missionData.program_type;
-    this.program = missionData.program;
-    this.requirements = missionData.requirements.map(function(r) { return new Requirement(r); });
-    this.label = function() {
-        var _this = this;
-        return _this.player + ' (' + _this.rating + '/' + _this.series + ') - ' + _this.program_type + ' - ' + _this.program;
-    };
+    var _this = this;
+    _this.id = missionData.id || missionData.value || 0;
+    _this.player = missionData.player || "";
+    _this.rating = missionData.rating || "";
+    _this.series = missionData.series || "";
+    _this.duration = missionData.duration || "";
+    _this.team = missionData.team || "";
+    _this.position = missionData.position || "";
+    _this.program_type = missionData.program_type || "";
+    _this.program = missionData.program || "";
+    _this.requirements = missionData.requirements.map(function(r) { return new Requirement(r); });
+    _this.label = _this.player + ' (' + _this.rating + '/' + _this.series + ') - ' + _this.program_type + ' - ' + _this.program;
 }
 
 function getMissionsFromData(missionsData) {
